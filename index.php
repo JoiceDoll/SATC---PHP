b77c91c036eeef248ddab5c9b2b5b069b836c922
Joice Doll <99621429+JoiceDoll@users.noreply.github.com>
2022-06-12 22:37:42 -0300

    second

--- index.php
+++ index.php
@@ -7,7 +7,20 @@ session_start();
         <meta charset="UTF-8">
         <title> Formulario de Contato</title>
     </head>
+
     <body>
+
+    <style>
+body{font-family:Arial,Helvetica; margin:0; padding:0;background-color:#244d61}
+.content{display:flex; justify-content:center; background-color:#fff; height:500px;}
+.form{display:flex;flex-direction:column; margin-top:40px;}
+.contato{width: 100%; max-width:500px;}
+.SendCadCont{background-color:#244d61; color:#fff;}
+h1{color:#244d61; text-align:center;}
+
+    </style>
+    <div class="content">
+        <div class="contato">
         <h1>Cadastrar Mensagem</h1>
         <?php
         if(isset($_SESSION['msg'])){
@@ -15,7 +28,7 @@ session_start();
             unset($_SESSION['msg']);
         }
         ?>
-        <form method="POST" action="form.php">
+        <form class="form" method="POST" action="form.php">
             <label>Nome: </label>
             <input type="text" name="nome" placeholder="Insira seu nome completo"><br><br>
             
@@ -28,7 +41,9 @@ session_start();
             <label>Mensagem: </label>
             <textarea name="mensagem" rows="3" cols="50"></textarea><br><br>
             
-            <input name="SendCadCont" type="submit" value="Cadastrar">
+            <input class="SendCadCont" name="SendCadCont" type="submit" value="Cadastrar">
         </form>
+    </div>
+    </div>
     </body>
 </html>
